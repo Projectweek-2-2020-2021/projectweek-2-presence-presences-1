5ecf9d493cc00f1da10dd7cc8d987a86c3aefdf5
Alexander-Symons <alexander.symons@student.ucll.be>
2020-12-14 14:31:55 +0100

    Spike Selenium Tests
    Spike Selenium tests en page objects werkt, let op in Config file Base URL aan gepast

--- src/test/java/ucll/project/ui/Config.java
+++ src/test/java/ucll/project/ui/Config.java
@@ -1,7 +1,7 @@
 package ucll.project.ui;
 
 public class Config {
-    public static final String BASE_URL = "http://localhost:8080/Controller";
+    public static final String BASE_URL = "http://localhost:8080/project_ucll_war_exploded/Controller";
 
 
 
