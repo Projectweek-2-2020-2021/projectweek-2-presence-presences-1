d605c76ca608ae68e8f305a82b97b2d98409c8a3
Alexander-Symons <alexander.symons@student.ucll.be>
2020-12-15 09:06:23 +0100

    out ge comment

--- src/main/java/ucll/project/ui/controller/LesOverviewLector.java
+++ src/main/java/ucll/project/ui/controller/LesOverviewLector.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /*
 public class LesOverviewLector extends RequestHandler {
-    
+
     public LesOverviewLector(String command, CountryService countryService) {
         super(command, countryService);
     }
